5964b452ac96884fc239de4c521ee8d96e6f9724
Frederik Vestre <freqmod@gmail.com>
2023-03-12 20:49:41 +0100

    Make helicoid server with helix editor compile and start

--- helicoid-helixserver/src/server.rs
+++ helicoid-helixserver/src/server.rs
@@ -1,4 +1,5 @@
 use anyhow::{anyhow, Result};
+use arc_swap::{access::Map, ArcSwap};
 use async_trait::async_trait;
 use helicoid_protocol::{
     caching_shaper::CachingShaper,
@@ -18,7 +19,8 @@ use helicoid_protocol::{
     },
     text::{FontEdging, FontHinting, ShapableString},
 };
-use helix_view::Editor;
+use helix_core::{config::user_syntax_loader, syntax};
+use helix_view::{editor::Config, graphics::Rect, theme, Editor};
 use ordered_float::OrderedFloat;
 use smallvec::{smallvec, SmallVec};
 use std::{net::SocketAddr, sync::Arc};
@@ -77,13 +79,14 @@ impl HelicoidServer {
         log::trace!("Helicoid test server event loop start");
         let mut state_data = ServerStateData {
             editor: self.editor.clone(),
+            compositor: Compositor {},
         };
         loop {
             log::trace!("Helicoid test server event loop iterate");
             tokio::select! {
                 result = TcpBridgeServer::wait_for_connection(self.bridge.clone(), &self.listen_address, state_data) =>{
                     /* Currently all event handling is done inside the state */
-                    state_data =  ServerStateData { editor: self.editor.clone()};
+                    state_data =  ServerStateData {editor:self.editor.clone(), compositor: Compositor {  } };
                 },
                 /* Maybe add select on program close-channel here to close cleanly */
             }
@@ -94,10 +97,39 @@ impl HelicoidServer {
 impl HelicoidEditor {
     pub fn new() -> Self {
         let (editor_state_changed_send, _) = broadcast::channel(1);
-
+        let exe_path = std::env::current_exe().unwrap();
+        let resource_path = exe_path
+            .as_path()
+            .parent()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .parent()
+            .unwrap();
+        let paths = vec![resource_path.join("user"), resource_path.join("default")];
+        let theme_loader = Arc::new(theme::Loader::new(&paths));
+        let syn_loader_conf = user_syntax_loader().unwrap();
+        let syn_loader = std::sync::Arc::new(syntax::Loader::new(syn_loader_conf));
+        //        let syn_loader = Arc::new(helix_core::syntax::Loader::new());
+        /*        let config = match std::fs::read_to_string(config_dir.join("config.toml")) {
+            Ok(config) => toml::from_str(&config)
+                //                .map(helix_term::keymap::merge_keys)
+                .unwrap_or_else(|err| {
+                    eprintln!("Bad config: {}", err);
+                    eprintln!("Press <ENTER> to continue with default config");
+                    use std::io::Read;
+                    // This waits for an enter press.
+                    let _ = std::io::stdin().read(&mut []);
+                    Config::default()
+                }),
+            Err(err) if err.kind() == std::io::ErrorKind::NotFound => Config::default(),
+            Err(err) => return Err(Error::new(err)),
+        };*/
+        let config = Arc::new(ArcSwap::from_pointee(Config::default()));
         Self {
             editor_state_changed_send,
-            text: String::new(),
+            editor: Editor::new(Rect::new(0, 0, 10, 10), theme_loader, syn_loader, config),
+            //            text: String::new(),
         }
     }
     pub fn update_receiver(&self) -> BReceiver<()> {
@@ -155,12 +187,12 @@ impl ServerState {
                     };
                     if let Some(text) = text {
                         let mut editor = self.state_data.editor.lock().await;
-                        editor.text += &text.to_string();
+                        //editor.text += &text.to_string();
                     }
                     if let VirtualKeycode::Backspace = event.virtual_keycode {
                         let mut editor = self.state_data.editor.lock().await;
-                        let textlen = editor.text.len().saturating_sub(1);
-                        editor.text.truncate(textlen);
+                        //let textlen = editor.text.len().saturating_sub(1);
+                        //editor.text.truncate(textlen);
                     }
                     self.sync_text().await?;
                 }
@@ -180,7 +212,7 @@ impl ServerState {
         shaper.set_font_key(3, String::from("MissingGlyphs"));
         shaper.set_font_key(4, String::from("LastResort-Regular"));
         let text = String::from("User input text");
-        let mut string_to_shape = ShapableString::from_text(&(text + &editor.text));
+        let mut string_to_shape = ShapableString::from_text(&(text));
         let font_paint = FontPaint {
             color: 0xFFCCCCCC,
             blend: helicoid_protocol::gfx::SimpleBlendMode::SrcOver,
