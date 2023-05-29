6092896a6fa6a9f05213d768a3aef2b0be38442f
Frederik Vestre <freqmod@gmail.com>
2023-05-29 13:06:33 +0200

    Initial implementation of tcp bridge sync

--- helicoid-protocol/src/tcp_bridge_sync.rs
+++ helicoid-protocol/src/tcp_bridge_sync.rs
@@ -1,6 +1,8 @@
+use anyhow::Result;
 use rkyv::ser::ScratchSpace;
 use rkyv::ser::{serializers::AllocSerializer, Serializer};
 use rkyv::{Archive, Deserialize, Infallible, Serialize};
+use std::io::{Read, Write};
 use std::{
     marker::PhantomData,
     net::{SocketAddr, TcpListener, TcpStream},
@@ -11,7 +13,9 @@ use std::{
 };
 
 use hashbrown::HashMap;
-use rkyv::ser::serializers::{AllocScratch, CompositeSerializer, WriteSerializer};
+use rkyv::ser::serializers::{
+    AlignedSerializer, AllocScratch, CompositeSerializer, WriteSerializer,
+};
 
 use crate::bridge_logic::SerializeWith;
 use crate::{
@@ -83,19 +87,88 @@ where
     let receive = TcpBridgeReceive::new(r)?;
     Ok((receive, send))
 }
+
+pub enum TcpBridgeReceiveError {
+    IoError(std::io::Error),
+    NoMoreData,
+}
+pub enum TcpBridgeSendError {
+    IoError(std::io::Error),
+}
 impl<M: Archive> TcpBridgeReceive<M>
 where
     M::Archived: Deserialize<M, rkyv::Infallible>,
 {
-    fn new(r: TcpStream) -> Result<Self, std::io::Error> {
-        todo!()
+    pub fn new(reader: TcpStream) -> Result<Self, std::io::Error> {
+        reader.set_nonblocking(true)?;
+        Ok(Self {
+            tcp_conn: reader,
+            processor: TcpBridgeReceiveProcessor::new(),
+        })
+    }
+    pub fn try_receive(&mut self) -> Result<M, TcpBridgeReceiveError> {
+        loop {
+            if let Some(buffer) = self.processor.next_read_buffer() {
+                let num_read = match self.tcp_conn.read(buffer) {
+                    Ok(num_read) => num_read,
+                    Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                        self.processor.mark_data_read(0);
+                        log::trace!("Read would block");
+                        return Err(TcpBridgeReceiveError::NoMoreData);
+                    }
+                    Err(e) => return Err(TcpBridgeReceiveError::IoError(e)),
+                };
+                self.processor.mark_data_read(num_read);
+                if num_read == 0 {
+                    break;
+                }
+                if let Some(archive) = self.processor.extract_archive() {
+                    return Ok(archive);
+                }
+            }
+        }
+        Err(TcpBridgeReceiveError::NoMoreData)
     }
 }
+
 impl<M> TcpBridgeSend<M>
 where
     M: SerializeWith,
 {
     fn new(writer: TcpStream) -> Result<Self, std::io::Error> {
-        todo!()
+        //writer.set_nonblocking(true)?;
+        Ok(Self {
+            tcp_conn: writer,
+            serializer: Default::default(),
+            dummy_serializer: Default::default(),
+            send_type: PhantomData,
+        })
+    }
+    fn send(&mut self, message: &mut M) -> Result<(), TcpBridgeSendError> {
+        let mut serializer = self.serializer.take().unwrap();
+        let mut dummy_serializer = self.dummy_serializer.take().unwrap();
+        message
+            .serialize(&mut serializer, &mut dummy_serializer)
+            .unwrap();
+        let (inner_serializer, scratch, shared) = serializer.into_components();
+        let mut bytes = inner_serializer.into_inner();
+        log::trace!("Tcp bridge Sending {} bytes ({:?})", bytes.len(), bytes);
+        match self.tcp_conn.write(&bytes) {
+            Ok(written) => {
+                assert!(written == bytes.len());
+            }
+            Err(e) => {
+                return Err(TcpBridgeSendError::IoError(e));
+            }
+        }
+        bytes.clear();
+        self.serializer = Some(TBSSerializer::new(
+            AlignedSerializer::new(bytes),
+            scratch,
+            shared,
+        ));
+        self.dummy_serializer = Some(dummy_serializer);
+
+        Ok(())
     }
 }
